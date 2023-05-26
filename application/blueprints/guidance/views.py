b959681a1223db8de1b0f5bf8b3e3025f5fb9f17
Adam Shimali <adam.shimali@gmail.com>
2023-05-26 15:38:17 +0100

    port introduction from html + mardown - html - no styling

--- application/blueprints/guidance/views.py
+++ application/blueprints/guidance/views.py
@@ -3,7 +3,33 @@ from flask import Blueprint, render_template
 guide = Blueprint("guidance", __name__, url_prefix="/guidance")
 
 
+themes = [
+    "Guidance for local planning authorities",
+    "Data specification guidance",
+    "Other guidance",
+]
+
+
+titles = {
+    "introduction": "Introduction for local planning authorities",
+}
+
+
 @guide.route("/")
 def guidance():
-    page_data = {"title": "Guidance for local planning authorities"}
-    return render_template("guidance/index.html", page_data=page_data)
+    page_data = {
+        "page_title": "Guidance for local planning authorities",
+        "themes": themes,
+        "page_name": "home",
+    }
+    return render_template("guidance/index.html", **page_data)
+
+
+@guide.route("/<string:page_name>")
+def page(page_name):
+    page_data = {
+        "page_title": titles[page_name],
+        "themes": themes,
+        "page_name": page_name,
+    }
+    return render_template(f"guidance/{page_name}.html", **page_data)
